597549146b9ec3e3a5dfd01460da3af810d20e60
Rei Yamahara <rei.yamahara@gmail.com>
2026-02-16 10:59:00 -0300

    past changes

--- strategy/RSI.py
+++ strategy/RSI.py
@@ -4,13 +4,18 @@ from strategy.strategy_base import StrategyBase
 
 
 class RSIStrategy(StrategyBase):
-    def __init__(self, period=14, oversold=30, overbought=70):
+    def __init__(self, period=14, oversold=30, overbought=70, entry_confirm_bars=2):
         super().__init__("RSI")
         self.period = period
         self.oversold = oversold
         self.overbought = overbought
+        self.entry_confirm_bars = max(1, int(entry_confirm_bars))
         self.prices = deque(maxlen=period + 1)
         self.position = 0.0
+        self.stop_loss_events = []
+        self._entry_streak = 0
+        self.last_rsi = np.nan
+        self.last_entry_streak = 0
 
     def _rsi(self):
         diffs = np.diff(np.array(self.prices))
@@ -28,13 +33,24 @@ class RSIStrategy(StrategyBase):
         self.prices.append(current_price)
 
         if len(self.prices) < self.period + 1:
+            self.last_rsi = np.nan
+            self.last_entry_streak = self._entry_streak
             return 0.0
 
         rsi = self._rsi()
+        self.last_rsi = rsi
 
         if rsi <= self.oversold:
+            self._entry_streak += 1
+        else:
+            self._entry_streak = 0
+
+        if self.position == 0.0 and self._entry_streak >= self.entry_confirm_bars:
             self.position = 1.0
         elif rsi >= self.overbought:
             self.position = 0.0
+            self._entry_streak = 0
+
+        self.last_entry_streak = self._entry_streak
 
         return self.position
